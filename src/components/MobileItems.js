52323993309905940afbcc58000ca6ed7458951c
esteloabell <manitobaaa@gmail.com>
2021-09-20 15:30:09 -0500

    Create MobileItems.js

---
+++ src/components/MobileItems.js
@@ -0,0 +1,63 @@
+import Boba from "../images/mobile/boba.png";
+import Crypto from "../images/mobile/crypto.png";
+import Delivery from "../images/mobile/delivery.png";
+import Furniture from "../images/mobile/furniture.png";
+import Movie from "../images/mobile/movie.png";
+import Recipe from "../images/mobile/recipe.png";
+import Travel from "../images/mobile/travel.png";
+import Trip from "../images/mobile/trip.png";
+
+
+export const MobileItems = [
+  {
+    title: 'Trip UI',
+    url: 'https://media4.giphy.com/media/bM0w2QJtD1zZFsvI3G/giphy.gif?cid=790b76119ec73b73b2ca8ffdacb7783542aee569afcb7cb8&rid=giphy.gif&ct=g',
+    mobileImage: Trip,
+    description: ['React Native UI',]
+  },
+  {
+    title: 'Travel UI',
+    url: 'https://media1.giphy.com/media/lGan8O2WUuU2lI94G2/giphy.gif?cid=790b7611a34f8c71d6bf3e4cd0b60671bf2db92b1d3a88ba&rid=giphy.gif&ct=g',
+    mobileImage: Travel,
+    description: ['React Native UI',]
+
+  },
+  {
+    title: 'Food Recipe UI',
+    url: 'https://media1.giphy.com/media/7wETJL5vkqhdFYc3ZV/giphy.gif?cid=790b7611c2b554a710f2050906c78bafb764fe28660597b2&rid=giphy.gif&ct=g',
+    mobileImage: Recipe,
+    description: ['React Native UI',]
+  },
+  {
+    title: 'Food Delivery UI',
+    url: 'https://media1.giphy.com/media/7wETJL5vkqhdFYc3ZV/giphy.gif?cid=790b7611c2b554a710f2050906c78bafb764fe28660597b2&rid=giphy.gif&ct=g',
+    mobileImage: Delivery,
+    description: ['React Native UI',]
+
+  },
+  {
+    title: 'Movie UI',
+    url: 'https://media2.giphy.com/media/0RqcoTLSWbR7fb3V2B/giphy.webp',
+    mobileImage: Movie,
+    description: ['React Native UI',]
+  },
+  {
+    title: 'Furniture UI',
+    url: 'https://media4.giphy.com/media/4k0o2WmW9qOi407HXb/giphy.webp',
+    mobileImage: Furniture,
+    description: ['React Native UI',]
+  },
+  {
+    title: 'Crypto UI',
+    url: 'https://media0.giphy.com/media/vgJGHFMDq6l2DwpB2N/giphy.webp',
+    mobileImage: Crypto,
+    description: ['React Native UI',]
+  },
+  {
+    title: 'Boba Tea UI',
+    url: 'https://media4.giphy.com/media/cDrnlmo8H7SrfmiKxB/giphy.webp',
+    mobileImage: Boba,
+    description: ['React Native UI',]
+
+  },
+]
