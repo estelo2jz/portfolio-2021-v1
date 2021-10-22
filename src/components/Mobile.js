5f768a0f17a27c0a7e25b8acc027a1de81f0e529
esteloabell <manitobaaa@gmail.com>
2021-10-22 10:01:27 -0500

    Update Mobile.js

--- src/components/Mobile.js
+++ src/components/Mobile.js
@@ -12,9 +12,9 @@ export default class Portfolio extends Component {
     return (
       <div className="mobile" id="mobile">
         <div className="mobile__header">
-          <div className="mobile__logo">
+          {/* <div className="mobile__logo">
             <img src={RNLogo} alt="react-native-logo" />
-          </div>
+          </div> */}
           <div className="mobile__exp">
             <p>
               My experience in building and implementing react native projects to improve my skills by learning on how to build better UI.
@@ -22,7 +22,7 @@ export default class Portfolio extends Component {
           </div>
         </div>
         <div className="mobile__project">
-          {/* {MobileItems.map((item, index, props) => {
+          {MobileItems.map((item, index, props) => {
             return (
               <a href={item.url} target="_blank" className="mobile__items__container">
                 <div key={index} className="mobile__url__container">
@@ -32,36 +32,15 @@ export default class Portfolio extends Component {
                 </div>
                 <div className="mobile__image__container">
                   <div className="mobile__image__self">
-                    <img src={item.mobileImage} style={{width: '300px'}} alt="mobile-items" />         
+                    {/* <img src={item.mobileImage} style={{width: '300px'}} alt="mobile-items" />          */}
+                    <video autoplay="true" muted loop="true">
+                      <source src={item.mobileImage} type="video/mp4" autoplay="true" muted loop="true" />
+                    </video>
                   </div>
                 </div>
               </a>
             )
-          })} */}
-          <div className="mobile__project__item">
-            <iframe src="https://giphy.com/embed/8ph80VfUkzlgrwrmfG" width="100%" height="100%" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
-          </div>
-          {/* <div className="mobile__project__item">
-            <iframe src="https://giphy.com/embed/geEjlGJbvEWie0QWfU" width="100%" height="100%" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
-          </div> */}
-          <div className="mobile__project__item">
-            <iframe src="https://giphy.com/embed/uRyOXxLzQt87JvTGED" width="100%" height="100%" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
-          </div>
-          {/* <div className="mobile__project__item">
-            <iframe src="https://giphy.com/embed/STDOQRg41pUSbzmAjK" width="100%" height="100%" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
-          </div> */}
-          <div className="mobile__project__item">
-            <iframe src="https://giphy.com/embed/jDAghfL5WNxjEULj3s" width="100%" height="100%" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
-          </div>
-          {/* <div className="mobile__project__item">
-            <iframe src="https://giphy.com/embed/ZbxtnVxPRgYwh6khRO" width="100%" height="100%" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
-          </div> */}
-          {/* <div className="mobile__project__item">
-            <iframe src="https://giphy.com/embed/1gKiWRXWLxKxIe2B9S" width="100%" height="100%" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
-          </div> */}
-          {/* <div className="mobile__project__item">
-            <iframe src="https://giphy.com/embed/qzcZjXyFm4LCdEjd0Y" width="100%" height="100%" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
-          </div> */}
+          })}
         </div>
       </div>
     )
