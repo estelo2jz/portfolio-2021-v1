96e5d0faf619acde92c572dbf25d3683e0b48123
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2022-07-13 23:02:58 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ export default class Footer extends Component {
           <div className="footer__left">
             <h2>Estelo Abellanosa</h2>
             <p>
-              Web Developer
+              Web Developer / Graphic Designer
             </p>
           </div>
           <div className="footer__right">
